80aeef3025d1c9437d849ae6db59303c17d01dec
Alexandro Gallardo <146788834+Ale-17-pe@users.noreply.github.com>
2025-09-19 18:28:02 -0500

    ProyectoMicroServicios

---
+++ src/test/java/com/arqdise/denuncias/DenunciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arqdise.denuncias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenunciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
